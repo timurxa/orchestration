f08841f7075a6d9c170e0a4aecd4b97cd1643227
ruskei <dominusfinem@gmail.com>
2026-09-11 20:46:32 -0700

    refactor: remove obsolete prototype code

--- src/vecherinka_tests.nim
+++ src/vecherinka_tests.nim
@@ -274,3 +274,28 @@ when defined(vecherinka_ir_tests):
     doAssert not compiles(fan(first_second, second_third))
     doAssert not compiles(first_second >>> first_third)
     doAssert not compiles(12 >>> first_second)
+
+
+# type
+#   ImplementationRequest = distinct string
+#   Codebase = distinct Location
+#   Issues = distinct seq[string]
+#   Audit = object
+#     case ok: bool
+#     of true: discard
+#     of false: issues: Issues
+# 
+# const cheap = luna.low
+# 
+# konez:
+#   > fix (Codebase, Issues) ~> Codebase:
+#     cheap[(Codebase, Issues), Codebase]("Read the issues and fix them in the codebase.")
+# 
+#   > audit (ImplementationRequest, Codebase) ~> Audit:
+#     cheap[(ImplementationRequest, Codebase), Audit]("Audit the codebase for issues based on the implementation request")
+# 
+#   > audit_fix_loop (ImplementationRequest, Codebase) ~> Codebase {.entry.}:
+#     (it &&& audit) >>> (so[Codebase](input) do:
+#       let ((req, code), audit) = input
+#       if audit.ok: pure(code)
+#       else: (req, (code, audit.issues)) >>> lift[(_, here)](fix) >>> audit_fix_loop)
